5b41c46a07fea272a0871054dd6ac8815c3a3b43
MICHAEL DAVID MORALES CUBILLO <mmorales16@gmail.com>
2023-08-01 23:39:08 -0600

    Validación de ErrorMessage

--- nomina/Controllers/UserController.cs
+++ nomina/Controllers/UserController.cs
@@ -40,19 +40,33 @@ namespace nomina.Controllers
         {
             try
             {
-                // Intenta insertar un nuevo usuario utilizando el método InsertUser del repositorio UserDAO
                 string result = userRepository.InsertUser(user);
-                Console.WriteLine("User added: " + result);
-                return RedirectToAction("Index");
+
+                if (result == "Success")
+                {
+                    // Redireccionar a la vista "Index" en caso de éxito
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    // Si la inserción falla, agregar un mensaje de alerta a la ViewBag
+                    ViewBag.ErrorMessage = "Error al insertar el usuario en la base de datos.";
+                }
             }
             catch (Exception ex)
             {
-                        
-                return View(user);
+                // En caso de excepción, agregar un mensaje de alerta a la ViewBag
+                ViewBag.ErrorMessage = "Ocurrió un error durante la inserción del usuario: " + ex.Message;
             }
 
-
+            // Devolver la vista "CreateUser" con los datos del usuario ingresados previamente
+            // y el mensaje de alerta (si corresponde).
+            var roles = userRepository.ReadRoles().ToList();
+            ViewBag.Roles = new SelectList(roles, "id", "Description");
+            return View(user);
         }
+
+
 
 
 
