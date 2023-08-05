a2a061ef66bced5ddd3553f38cd9eac02f9ab0a3
VegaGuevara <116321138+VegaGuevara@users.noreply.github.com>
2023-08-04 20:32:38 -0600

    OvertimeController added

---
+++ nomina/Controllers/OvertimeController.cs
@@ -0,0 +1,27 @@
+﻿using nomina.Models.DAO;
+using System.Web.Mvc;
+
+
+
+namespace nomina.Controllers
+{
+    public class OvertimeController : Controller
+    {
+
+
+
+        private OvertimeDAO overtimeRepository = new OvertimeDAO();
+        public ActionResult ReadOvertime()
+        {
+
+
+
+            return View(overtimeRepository.ReadOvertime());
+        }
+
+
+
+
+
+    }
+}
